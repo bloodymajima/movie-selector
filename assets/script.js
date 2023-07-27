b1c0baa7f63aef8d617a9a7544ffe3d6e88208bf
lukehevey <lukehevey8@gmail.com>
2023-07-26 19:34:04 -0500

    Added TODO comments in JS

---
+++ assets/script.js
@@ -0,0 +1,10 @@
+// Nav bar link for movie quiz takes you to the start page, genre link takes you to movie genres if thats all you want
+
+// When start button is pressed, it goes to the first question. hide class is removed for next question. 
+
+// When a question shows up, hide class is added for previous question, removed for current
+
+// Input from questions goes into function for selecting movies
+
+// Create card for the reccomended movie, trailer button takes to youtube, new button runs function for selecting movie, watch on buttons show what streaming service 
+
