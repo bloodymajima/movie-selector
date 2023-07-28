c6378075c2b4e2e17edc6bbc7dca34aa377473c9
lukehevey <lukehevey8@gmail.com>
2023-07-27 21:23:20 -0500

    dropdown in JS

--- assets/script.js
+++ assets/script.js
@@ -57,6 +57,31 @@ const streamingOnButton = document.getElementById('watch-on')
 
 const restartQuiz = document.getElementById('start-over')
 
+
+
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+    document.getElementById("dropdownMenu").classList.toggle("show");
+  }
+  
+  // Close the dropdown menu if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropdownBtn')) {
+      var dropdown = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdown.length; i++) {
+        var openDropdown = dropdown[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
+
+
+
+
 homePage.classList.remove('hide')
 
 startButton.addEventListener('click', startQ)
