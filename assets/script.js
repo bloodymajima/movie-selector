7e8a224247abe949c6d9e5dcadd6396a2a25f75e
lukehevey <lukehevey8@gmail.com>
2023-07-31 12:18:35 -0500

    Added number in front of questions in JS

--- assets/script.js
+++ assets/script.js
@@ -1,27 +1,27 @@
 // Variables
 
 var questions = [
-    {
-        question: "1. What is your mood today?",
-        answer: "Happy" || "Neutral" || "Sad",
-        choices: ["Happy", "Neutral", "Sad"]
-    },
-    {
-        question: "2. Question 2",
-        choices: ["Choice 1", "Choice 2", "Choice 3", "Choice 4"]
-    },
-    {
-        question: "3. Question 3",
-        choices: ["Choice 1", "Choice 2", "Choice 3", "Choice 4"]
-    },
-    {
-        question: "4. Question 4",
-        choices: ["Choice 1", "Choice 2", "Choice 3", "Choice 4"]
-    },
-    {
-        question: "5. Question 5",
-        choices: ["Choice 1", "Choice 2", "Choice 3", "Choice 4"]
-    }
+  {
+    question: "1. What is your mood today?",
+    answer: "Happy" || "Neautral" || "Sad",
+    choices: ["Happy", "Neutral", "Sad"]
+  },
+  {
+    question: "2. Question 2",
+    choices: ["Choice 1", "Choice 2", "Choice 3", "Choice 4"]
+  },
+  {
+    question: "3. Question 3",
+    choices: ["Choice 1", "Choice 2", "Choice 3", "Choice 4"]
+  },
+  {
+    question: "4. Question 4",
+    choices: ["Choice 1", "Choice 2", "Choice 3", "Choice 4"]
+  },
+  {
+    question: "5. Question 5",
+    choices: ["Choice 1", "Choice 2", "Choice 3", "Choice 4"]
+  }
 
 ]
 
@@ -61,22 +61,22 @@ const restartQuiz = document.getElementById('start-over')
 
 // Nav bar link for movie quiz takes you to the start page, genre link takes you to movie genres if thats all you want
 function myFunction() {
-    document.getElementById("dropdownMenu").classList.toggle("show");
-  }
-  
-  // Close the dropdown menu if the user clicks outside of it
-  window.onclick = function(event) {
-    if (!event.target.matches('.dropdownBtn')) {
-      var dropdown = document.getElementsByClassName("dropdown-content");
-      var i;
-      for (i = 0; i < dropdown.length; i++) {
-        var openDropdown = dropdown[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
-        }
+  document.getElementById("dropdownMenu").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function (event) {
+  if (!event.target.matches('.dropdownBtn')) {
+    var dropdown = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdown.length; i++) {
+      var openDropdown = dropdown[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
       }
     }
   }
+}
 
 
 
@@ -89,17 +89,18 @@ startButton.addEventListener('click', showQ)
 // }
 
 function showQ() {
-    questionContainer.innerHTML = ""
-    var question = document.createElement("h2");
-    question.innerHTML = questions[count].question;
-    questionContainer.append(question)
-    for (var i = 0; i < questions[count].choices.length; i++) {
-        var button = document.createElement("button")
-        button.textContent = questions[count].choices[i]
-        button.value = questions[count].choices[i]
-        questionContainer.append(button)
-    }
+  questionContainer.innerHTML = ""
+  var question = document.createElement("h2");
+  question.innerHTML = questions[count].question;
+  questionContainer.append(question)
+  for (var i = 0; i < questions[count].choices.length; i++) {
+    var button = document.createElement("button")
+    button.textContent = questions[count].choices[i]
+    button.value = questions[count].choices[i]
+    questionContainer.append(button)
+  }
 }
+
 
 //How to define variables using jQuery?
 
