06f19671e0afc915747170ea7e4771868f326554
lukehevey <lukehevey8@gmail.com>
2023-07-31 20:39:33 -0500

    Updated

---
+++ assets/script2.js
@@ -0,0 +1,3 @@
+// Generate button fetches random movie from api
+
+// A card exactly the same as the homepage pops up with a movie recommendation, possibly a button for a trailer or related vidoe, then a button to run the function again
